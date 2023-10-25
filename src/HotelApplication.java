c36c25be4eb16de0136a8ad57aac49f08a09bdc3
junxtar <aa1870088@naver.com>
2023-10-25 15:45:48 +0900

    feat: HotelApplication 클래스 구현

--- src/HotelApplication.java
+++ src/HotelApplication.java
@@ -1,4 +1,8 @@
+import context.ApplicationContext;
+
 public class HotelApplication {
    public static void main(String[] args) {
+      ApplicationContext application = new ApplicationContext();
+      application.run();
    }
 }
