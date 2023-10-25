7957b1ef682b6e03bf683c746b09f812b632cdd6
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2023-10-25 21:21:56 +0900

    "manager service 1"

--- src/context/ManagerContext.java
+++ src/context/ManagerContext.java
@@ -1,6 +1,40 @@
 package context;
 
-public class ManagerContext {
+import hotel.HotelService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class ManagerContext extends UtilContext {
    public void displayManagerMode() {
+      managerInputHandling();
+   }
+   public void managerInputHandling(){
+      lineWithText("매니저 모드");
+      System.out.println("1. 예약 현황.");
+      System.out.println("2. 수익 현황.");
+      int command = sc.nextInt();
+      switch (command) {
+         case 1 -> reservationStatus();
+         case 2 -> assertStatus();
+         default -> managerInputHandling();
+      }
+   }
+
+   public void reservationStatus(){
+      System.out.println("1. 빈객실 찾기.");
+      System.out.println("2. 예약 찾기.");
+      System.out.println("3. 오늘 객실 현황.");
+      int command = sc.nextInt();
+//      switch (command) {
+//         case 1 -> ;
+//         case 2 -> ;
+//         case 3 -> ;
+//         default -> managerInputHandling();
+//      }
+   }
+
+   public void assertStatus(){
+
    }
 }
