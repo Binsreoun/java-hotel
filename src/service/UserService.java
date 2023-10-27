2b0214d508900a8db246bb28d112b59d19e62da5
junxtar <aa1870088@naver.com>
2023-10-27 16:58:06 +0900

    feat: 유저 포인트 차감 기능 구현

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -7,4 +7,7 @@ public class UserService {
     public void chargePoint(User user, int point) {
         user.setPoint(user.getPoint() + point);
     }
+    public void deductPoint(User user, int point) {
+        user.setPoint(user.getPoint() - point);
+    }
 }
